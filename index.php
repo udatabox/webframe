79875edc2ab3e7e2727c2cb9850b20e9f3246979
UDATABOX® Software <info@udatabox.com>
2015-04-21 15:47:20 +0200

    #2 imagenes agregadas

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Web Frame, Inc.</title>
+	<link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+</head>
+<body>
+	
+</body>
+</html>
